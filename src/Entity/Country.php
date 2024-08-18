0bb8f84ae7cfc21b80b4609c5d0e140ab9e78f2f
basharsamman51 <159685891+basharsamman51@users.noreply.github.com>
2024-08-18 21:25:54 +0300

    Feat: Country entity setup completed [DONE]

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -3,29 +3,146 @@ declare(strict_types=1);
 
 namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+use Doctrine\DBAL\Types\Types;
+
 
 class Country
 {
 
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private int $id;
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $region = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $subRegion = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $demonym = null;
+
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?string $population = null;
+
+    #[ORM\Column]
+    private ?bool $independant = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $flag = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?array $currency = null;
 
     /**
      * @return int
      */
-    public function getId(): int
+     public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
-    public function setId(int $id): void
+    public function getName(): ?string
     {
-        $this->id = $id;
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    public function setRegion(string $region): static
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    public function getSubRegion(): ?string
+    {
+        return $this->subRegion;
+    }
+
+    public function setSubRegion(string | null $subRegion): static
+    {
+        $this->subRegion = $subRegion;
+
+        return $this;
+    }
+
+    public function getDemonym(): ?string
+    {
+        return $this->demonym;
+    }
+
+    public function setDemonym(string | null $demonym): static
+    {
+        $this->demonym = $demonym;
+
+        return $this;
+    }
+
+    public function getPopulation(): ?string
+    {
+        return $this->population;
+    }
+
+    public function setPopulation(string $population): static
+    {
+        $this->population = $population;
+
+        return $this;
+    }
+
+    public function isIndependant(): ?bool
+    {
+        return $this->independant;
+    }
+
+    public function setIndependant(bool $independant): static
+    {
+        $this->independant = $independant;
+
+        return $this;
+    }
+
+    public function getFlag(): ?string
+    {
+        return $this->flag;
+    }
+
+    public function setFlag(string $flag): static
+    {
+        $this->flag = $flag;
+
+        return $this;
+    }
+
+    public function getCurrency(): ?array
+    {
+        return $this->currency;
+    }
+
+    public function setCurrency(?array $currency): static
+    {
+        $this->currency = $currency;
+
+        return $this;
     }
 
 }
