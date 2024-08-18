894083badf974b89dca2e0135f9ba20301a30e71
basharsamman51 <159685891+basharsamman51@users.noreply.github.com>
2024-08-18 22:23:13 +0300

    Feat: CountrySyncCommand execution [DONE]

--- src/Command/CountrySyncCommand.php
+++ src/Command/CountrySyncCommand.php
@@ -3,12 +3,22 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Service\CountryService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class CountrySyncCommand extends Command
 {
+    private $doctrine;
+    protected $countryService;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+        $this->countryService = new CountryService();
+    }
+
     protected function configure(): void
     {
         $this->setName('countries:sync');
@@ -17,6 +27,9 @@ class CountrySyncCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        return COMMAND::SUCCESS;
+        $entityManager = $this->doctrine->getManager();
+        $result = $this->countryService->syncData($entityManager);
+        
+        return $result?COMMAND::SUCCESS:COMMAND::FAILURE;
     }
 }
--- src/Service/CountryService.php
+++ src/Service/CountryService.php
@@ -2,6 +2,9 @@
 
 namespace App\Service;
 
+use App\Entity\Country;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\HttpClient\HttpClient;
 
 class CountryService 
@@ -17,5 +20,55 @@ class CountryService
     {
         $response = $this->client->request("GET","all");
         return $response->toArray();
+    }
+
+    /**
+     * this method is emptying the countries table and readding the entries from the api base on the batchSize
+     */
+    public function syncData($entityManager)
+    {
+        $data = $this->fetchCountries();
+        $qb = $entityManager->createQueryBuilder();
+        $qb->delete(Country::class, 'e');
+        $query = $qb->getQuery();
+        $query->execute();
+        $entityManager->getConnection()->beginTransaction(); 
+        try {
+            $batchSize = 20;
+            for ($i = 0; $i <= count($data)-1; ++$i) {
+                $country = new Country();
+                $country->setName($data[$i]['name']['official']);
+                $country->setRegion($data[$i]['region']);
+                $country->setSubRegion($data[$i]['subregion']??null);
+                $country->setPopulation($data[$i]['population']);
+                // $country->setIndependant($data[$i]['independent']);
+                $country->setFlag($data[$i]['flags']['png']);
+                // $country->setDemonym($data[$i]['demonyms']['eng']['m']);
+                $country->setDemonym(
+                    array_key_exists("demonyms",$data[$i])==true?
+                    $data[$i]['demonyms']['eng']['m']
+                    :null
+                );
+                // array_keys((array)$data[$i])[0];
+                $country->setCurrency(
+                    array_key_exists("currencies",$data[$i])==true?
+                    ['name'=>$data[$i]['currencies'][array_keys((array)$data[$i]['currencies'])[0]]['name'],'symbol'=>$data[$i]['currencies'][array_keys((array)$data[$i]['currencies'])[0]]['symbol']]
+                    :null
+                );
+                $entityManager->persist($country);
+                if (($i % $batchSize) === 0) {
+                    $entityManager->flush();
+                    $entityManager->clear();
+                }
+            }
+            $entityManager->flush(); 
+            $entityManager->clear();
+            $entityManager->getConnection()->commit();
+            return true;
+        } catch (Exception $e) {
+            $entityManager->getConnection()->rollBack();
+            throw $e;
+            return false;
+        }
     }
 }
